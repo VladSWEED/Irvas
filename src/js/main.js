b7301fcfbe599acae34a3e07fe970684e0908b1c
Vladislav Sviderski <svidvlad15@gmail.com>
2021-06-04 16:24:48 +0300

    tabs finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import "./slider"
 import modal from './modules/modalsMyProject'
+import tabs from './modules/tabs'
   window.addEventListener('DOMContentLoaded', () => {
     modal();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   })
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,4 +1,33 @@
-const tabs = (headerSelector, tabSelector, ) => {
-  
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector),
+  tabs = document.querySelectorAll(tabSelector),
+  content = document.querySelectorAll(contentSelector)
+
+  const hideContent = () => {
+    content.forEach(item => item.style.display = 'none')
+    tabs.forEach(item => item.classList.remove(activeClass))
+  }
+  const showContent = (i = 0) => {
+    content[i].style.display = 'block';
+    tabs[i].classList.add(activeClass);
+  }
+
+  hideContent();
+  showContent();
+
+  header.addEventListener('click', e => {
+    const target = e.target;
+
+    if (target && (target.classList.contains(tabSelector.replace(/\./, "")) || target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+      console.log('1 условие')
+      tabs.forEach((item, i) => {
+        if (item == target || target.parentNode == item) {
+          console.log('2 условие')
+          hideContent();
+          showContent(i);
+        }
+      })
+    }
+  })
 }
 export default tabs
